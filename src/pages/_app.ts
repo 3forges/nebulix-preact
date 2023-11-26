94da0c59aea7d6a198de0abddc73f17a7a77f94b
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-11-27 00:30:24 +0100

    I removed all occurences of Popper its done

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,9 +1,9 @@
 import type { App } from 'vue';
-import Popper from "vue3-popper"; 
+//import Popper from "vue3-popper"; 
 import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify';
 
 export default (app: App) => { 
-    app.component("Popper", Popper);
+    //app.component("Popper", Popper);
     app.use( Vue3Toasity,
   {
     autoClose: 3000,
